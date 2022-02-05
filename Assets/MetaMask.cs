27c1f3b6116a1ce14d154901d6ec323409ad3c4b
Mahmut Can Gönül <75094927+MahmutCanGonul@users.noreply.github.com>
2022-02-05 14:29:36 +0300

    Added CreateWallet on Web3 Function

--- Assets/MetaMask.cs
+++ Assets/MetaMask.cs
@@ -1,18 +1,21 @@
 using Nethereum.Web3;
+using NBitcoin;
+using Nethereum.HdWallet;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System;
+using System.Security.Cryptography;
+using System.Text;
 
 public class MetaMask : MonoBehaviour
 {
     // Start is called before the first frame update
     private Web3 web3;
-
     void Start()
     {
         web3 = new Web3(TestNethereum.url);
-         
+
         if (PlayerPrefs.HasKey("address") && PlayerPrefs.HasKey("privatekey"))
         {
             Debug.Log("Auto Login");
@@ -20,15 +23,19 @@ public class MetaMask : MonoBehaviour
         }
         else
         {
-            var address = "Adress";
+            var address = "Address";
             var priKey = "PrivateKey";
             LoginWeb3(address, priKey);
         }
 
+        //CreateWallet("Test123456");
+
+
+
 
     }
 
-    private void LoginWeb3(string address,string privateKey)
+    private void LoginWeb3(string address, string privateKey)
     {
         if (!string.IsNullOrEmpty(address) && !string.IsNullOrEmpty(privateKey))
         {
@@ -47,8 +54,8 @@ public class MetaMask : MonoBehaviour
                     }
                     //All request are success!
                     Debug.Log("Balance: " + Web3.Convert.FromWei(balance.Result.Value) + " ETH");
-                    PlayerPrefs.SetString("address",address);
-                    PlayerPrefs.SetString("privatekey",privateKey);
+                    PlayerPrefs.SetString("address", address);
+                    PlayerPrefs.SetString("privatekey", privateKey);
                 }
                 else
                 {
@@ -56,7 +63,7 @@ public class MetaMask : MonoBehaviour
                 }
 
             }
-            catch(Exception ex)
+            catch (Exception ex)
             {
                 Debug.Log(ex.Message);
             }
@@ -67,5 +74,45 @@ public class MetaMask : MonoBehaviour
         }
     }
 
-   
+    private void CreateWallet(string password)
+    {
+        if (password.Length > 6)
+        {
+            Mnemonic mnemo = new Mnemonic(Wordlist.English, WordCount.Twelve);
+            var secretWords = "";
+            for (int i = 0; i < mnemo.Words.Length; i++)
+            {
+                Debug.Log(mnemo.Words[i]);
+                secretWords += mnemo.Words[i] + " ";
+            }
+            var wallet = new Wallet(secretWords, password);
+            if (wallet != null)
+            {
+                var account = wallet.GetAccount(0);
+                Debug.Log("Account is created!");
+                Debug.Log("Adress: " + account.Address);
+                Debug.Log("PrivateKey: " + account.PrivateKey);
+                
+                //Save the all keys on local device
+                PlayerPrefs.SetString("address",account.Address);
+                PlayerPrefs.SetString("privatekey", account.PrivateKey);
+                PlayerPrefs.SetString("secretwords", secretWords);
+            }
+            else
+            {
+                Debug.Log("Something get issue!");
+            }
+
+        }
+        else
+        {
+            Debug.Log("Password is too weak!");
+        }
+
+
+    }
+
+
+
+
 }
